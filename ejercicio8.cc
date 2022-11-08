b385a4faae2bacc89478c76503a4fd5ff1afb344
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-08 09:31:30 +0100

    Delete ejercicio8.cc

--- ejercicio8.cc
+++
@@ -1,5 +0,0 @@
-/*
-    usuario_vms@pto0612:~$ id
-        uid=1565(usuario_vms) gid=100(users) grupos=100(users),20(dialout),24(cdrom),25(floppy),29(audio),30(dip),44(video),46(plugdev),108(netdev),113(bluetooth),118(scanner),126(sambashare),132(render),138(vboxusers),1025(vmwareusers)
-
-*/
