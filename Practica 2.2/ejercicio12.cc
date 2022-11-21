d0243ba1a2c946f3cbb2972990b197fd969272a4
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-21 10:41:06 +0100

    Add files via upload

---
+++ Practica 2.2/ejercicio12.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    int fd = open(argv[1], O_RDWR);
+    dup2(fd, 2); //stderr
+    dup2(fd, 1); //stdout
+    cout << "Holaaa" << endl << "Esto es una prueba de redicreccion" << endl;
+}
+
