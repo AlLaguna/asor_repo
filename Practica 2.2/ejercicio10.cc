3141a1eab35965802ab40fb79bce1776bae538c9
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-21 10:09:29 +0100

    Update ejercicio10.cc

--- Practica 2.2/ejercicio10.cc
+++ Practica 2.2/ejercicio10.cc
@@ -8,4 +8,8 @@ $ ls -i
 5772375 direct  5772139 prueba
 5772376 ej1     5769862 ejercicio1.cc
 
+
+//Cuando se elimina un fichero que tine un enlace simbolico, el enlace deja de funcionar (en la terminal te lo muestra en rojo y negro)
+//Cuando se elimina un ichero que tiene un enlace rigido, el "nuevo" fichero sigue funcionando y mostrando el contenido sin problema
+
 */
