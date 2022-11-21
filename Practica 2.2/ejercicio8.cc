0958753bdcba8067dee7295c3bec80a0ad1bbf08
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-21 09:45:04 +0100

    Add files via upload

---
+++ Practica 2.2/ejercicio8.cc
@@ -0,0 +1,39 @@
+/*
+ stat [OPTION]... FILE...
+
+Display file or file system status.
+
+       Mandatory  arguments  to  long  options are mandatory for short options
+       too.
+
+       -L, --dereference
+              follow links
+
+       -f, --file-system
+              display file system status instead of file status
+
+       --cached=MODE
+              specify how to use cached attributes; useful on remote file sys‐
+              tems. See MODE below
+
+       -c  --format=FORMAT
+              use  the  specified FORMAT instead of the default; output a new‐
+              line after each use of FORMAT
+
+       --printf=FORMAT
+              like --format, but interpret backslash escapes, and do not  out‐
+              put a mandatory trailing newline; if you want a newline, include
+              \n in FORMAT
+
+       -t, --terse
+              print the information in terse form
+
+       --help display this help and exit
+
+       --version
+              output version information and exit
+
+
+
+
+*/
