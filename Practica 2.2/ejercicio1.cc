ce8522b6b895f868c16ce9c6a3605f1795a74189
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-21 09:18:14 +0100

    Add files via upload

---
+++ Practica 2.2/ejercicio1.cc
@@ -0,0 +1,30 @@
+/*
+-a, --all
+              do not ignore entries starting with .
+
+-d, --directory
+              list directories themselves, not their contents
+
+-l     use a long listing format
+
+-h, --human-readable
+              with -l and -s, print sizes like 1K 234M 2G etc.
+
+-i, --inode
+              print the index number of each file
+
+-R, --recursive
+              list subdirectories recursively
+
+-1     list one file per line.  Avoid '\n' with -q or -b
+
+
+-F, --classify
+              append indicator (one of /=>@|) to entries
+
+
+--color[=WHEN]
+              colorize  the output; WHEN can be 'always' (default if omitted),
+              'auto', or 'never'; more info below
+
+*/
