ba2388aafbcccdc92b15db9d2ec6b156109f3371
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-21 10:06:22 +0100

    Add files via upload

---
+++ Practica 2.2/ejercicio10.cc
@@ -0,0 +1,11 @@
+/*
+$ ls -l
+lrwxrwxrwx 1 usuario_vms users     6 nov 21 10:04 direct -> prueba
+lrwxrwxrwx 1 usuario_vms users    13 nov 21 10:04 ej1 -> ejercicio1.cc
+
+
+$ ls -i
+5772375 direct  5772139 prueba
+5772376 ej1     5769862 ejercicio1.cc
+
+*/
