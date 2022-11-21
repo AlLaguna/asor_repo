2ed7e3ac891ef963d9006f3a21cf38b7133a586b
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-21 10:40:37 +0100

    Add files via upload

--- Practica 2.2/ejercicio12.cc
+++ Practica 2.2/ejercicio12.cc
@@ -12,7 +12,10 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int fd = open(argv[1], O_RDWR);
+    int fd1 = open(argv[2], O_RDWR);
     dup2(fd, 2); //stderr
     dup2(fd, 1); //stdout
     cout << "Holaaa" << endl << "Esto es una prueba de redicreccion" << endl;
 }
+
+//Depende del orden en el que se redireccionan de izq a der
