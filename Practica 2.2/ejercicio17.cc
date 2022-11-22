0f6b2031cc932da194c3091b42a25dd1f7762341
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-22 12:09:54 +0100

    Add files via upload

---
+++ Practica 2.2/ejercicio17.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+
+using namespace std;
+
+
+int main(int numArg, const char *argv[])
+{
+    if(numArg != 2)
+    {
+        cout << "ERROR ARGUMENTOS" << endl;
+    }
+    else
+    {
+        DIR *directorio = opendir(argv[1]);
+
+        if(directorio == NULL)
+        {
+            cout << "ERROR DIRECTORIO" << endl;
+        }
+        else{
+
+            struct dirent *d;
+
+            while((d = readdir(directorio)) != NULL)
+            {
+                if(d->d_type == DT_DIR)//Directorio  /
+                {
+                    cout << "/ " << d->d_name << endl;
+                }
+                else if(d->d_type == DT_REG)//fichero regular con permiso de ejecucion
+                {
+                    struct stat buf;
+                    stat(d->d_name, &buf);
+
+                    if(buf.st_mode == S_IXUSR || buf.st_mode == S_IXOTH || buf.st_mode == S_IXGRP)
+                    {
+                        cout << "* " << d->d_name << endl;
+                    }
+                    else
+                    {
+                        cout << d->d_name << endl;
+                    }
+
+                    
+                }
+                else if(d->d_type == DT_LNK) //Enlace simbolico ->
+                {
+                    char linkname[256];
+                    ssize_t r = readlink(d->d_name, linkname, 256);
+                    cout << d->d_name << " -> " << linkname << endl;
+                }       
+                
+
+            }
+        }
+    }
+    cout << endl;
+}
