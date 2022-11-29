c04232209d6cabcaedadbd97e9747c3b894cc550
AlLaguna <79445367+AlLaguna@users.noreply.github.com>
2022-11-29 12:08:12 +0100

    Add files via upload

---
+++ Practica 2.3/ej7sys.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	pid_t pid = fork();
+	
+    if(pid == 0)
+    {
+        system(argv[1]);
+        cout << "ejecutado execvp" << endl;
+		
+    }
+    else
+    {	
+		waitpid(pid, NULL, 0);
+		cout << "termino" << endl;
+	}
+	
+	return 0;
+}
